4bdee52b9d17b64e337dc7f703a88f144b8dc843
ArnoldoRicardo <anyback@gmail.com>
2021-07-28 20:37:10 -0500

    update menu

--- App.js
+++ App.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/components/Index'
-import { Menu } from './src/components/Menu'
+// delete for production
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import HomeStackScreen from "./src/components/Calendario";
+import SettingsStackScreen from "./src/components/Talleres";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <>
-      <View style={styles.menu}>
-      <Menu />
-        </View>
-      <View style={styles.container}>
-          <Main />  
-        </View>
-      </>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === "Calendario") {
+              return (
+                <MaterialIcons
+                  name="event-available"
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === "Talleres") {
+              return (
+                <MaterialIcons name="group-work" size={size} color={color} />
+              );
+            } else if (route.name === "Expociciones") {
+              return (
+                <FontAwesome name="paint-brush" size={size} color={color} />
+              );
+            } else if (route.name === "Resintos") {
+              return (
+                <MaterialCommunityIcons
+                  name="google-maps"
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === "Rutas") {
+              return (
+                <MaterialIcons name="add-road" size={size} color={color} />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Calendario" component={HomeStackScreen} />
+        <Tab.Screen name="Talleres" component={SettingsStackScreen} />
+        <Tab.Screen name="Expociciones" component={SettingsStackScreen} />
+        <Tab.Screen name="Resintos" component={SettingsStackScreen} />
+        <Tab.Screen name="Rutas" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  menu: {
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: 'red',
-  },container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Calendario/index.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { Button, Text, View } from "react-native";
+import React from "react";
+
+const HomeStack = createStackNavigator();
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+export default HomeStackScreen;
